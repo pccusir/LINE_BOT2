13fb2189894ad95c29c0b4e50a4cb955f2593913
pccusir <wfx2@ulive.pccu.edu.tw>
2025-05-10 14:01:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def handle_message(event):
     try:
         QA_answer = QA_response(msg)
         print(QA_answer)
-        if QA_answer!='No good match found in KB':
+        if QA_answer!='請確認您的學測號碼':
             line_bot_api.reply_message(event.reply_token, TextSendMessage(QA_answer))
     except:
         print(traceback.format_exc())
